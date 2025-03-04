cd6eadba238672552d00907eca7003289f95af5f
Joseph Oliveira Corrêa <joseph.oliveira.correa@gmail.com>
2025-03-04 17:03:22 -0300

    Push

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -2,32 +2,45 @@ extends RigidBody2D
 
 class_name Animal
 
-#==========================================================#
-#======================= POSITION =========================#
-#==========================================================#
-
-var starting_position: Vector2 = Vector2.ZERO
+@onready var arrow_sound_player: AudioStreamPlayer2D = $ArrowSoundPlayer
 
 #==========================================================#
 #========================= STATE ==========================#
 #==========================================================#
 
 enum STATE {READY, DRAG, RELEASE}
-var state: STATE = STATE.READY
+var _state: STATE = STATE.READY: get = get_state, set = set_state
+
+#==========================================================#
+#======================= POSITION =========================#
+#==========================================================#
+
+var _starting_position: Vector2 = Vector2.ZERO
 
 #==========================================================#
 #========================== DRAG ==========================#
 #==========================================================#
 
 const DRAG_LIMIT: Vector2 = Vector2(-60, 60)
-var drag_starting_position: Vector2 = Vector2.ZERO
+
+var _drag_starting_position: Vector2 = Vector2.ZERO:
+	get:
+		return _drag_starting_position
+	set(gmp):
+		_drag_starting_position = gmp
+
+var _dragged_position: Vector2 = Vector2.ZERO:
+	get:
+		var aux_dragged_distance = get_global_mouse_position() - _drag_starting_position
+		var aux_clamped_dragged_distance = aux_dragged_distance.clampf(DRAG_LIMIT.x,DRAG_LIMIT.y)
+		return _starting_position + aux_clamped_dragged_distance
 
 #==========================================================#
 #========================= READY ==========================#
 #==========================================================#
 
 func _ready() -> void:
-	starting_position = position
+	_starting_position = position
 
 #==========================================================#
 #==================== PHYSICS PROCESS =====================#
@@ -37,7 +50,7 @@ func _physics_process(_delta: float) -> void:
 	state_process()
 
 func state_process() ->void:
-	match state:
+	match _state:
 		STATE.READY:
 			pass
 		STATE.DRAG:
@@ -49,25 +62,27 @@ func state_process() ->void:
 #========================= STATE ==========================#
 #==========================================================#
 
+func get_state() -> STATE:
+	return _state
+
 func set_state(value) -> void:
-	state = value
-	match state:
+	_state = value
+	match _state:
 		STATE.RELEASE:
 			freeze = false
 		STATE.DRAG:
-			drag_starting_position = get_global_mouse_position()
+			_drag_starting_position = get_global_mouse_position()
 
 #==========================================================#
 #========================= READY ==========================#
 #==========================================================#
 
 func on_click(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if was_ready(event):
-		set_state(STATE.DRAG)
+	if is_ready(event):
+		_state = STATE.DRAG
 	
-
-func was_ready(event)->bool:
-	return state == STATE.READY and event.is_action_pressed("click")
+func is_ready(event)->bool:
+	return _state == STATE.READY and event.is_action_pressed("click")
 
 #==========================================================#
 #========================= DRAG ===========================#
@@ -75,25 +90,31 @@ func was_ready(event)->bool:
 
 func dragging()-> void:
 	if has_stopped_dragging():
-		set_state(STATE.RELEASE)
+		_state = STATE.RELEASE
 		return
 	
 	drag()
 
-func has_stoped_dragging()-> bool:
-	return state == STATE.DRAG and Input.is_action_just_released("click")
-
 func drag()->void:
-	position = get_computed_position()
+	play_arrow_sound()
+	position = _dragged_position
 
-func get_dragged_distance()-> Vector2:
-	return get_global_mouse_position() - drag_starting_position
+func has_stopped_dragging()-> bool:
+	return _state == STATE.DRAG and Input.is_action_just_released("click")
 
-func get_clamped_dragged_distance()-> Vector2:
-	return get_dragged_distance().clampf(DRAG_LIMIT.x,DRAG_LIMIT.y)
+func was_dragged()-> bool:
+		if(position != _dragged_position):
+			return true
+		return false
 
-func get_computed_position()-> Vector2:
-	return starting_position + get_clamped_dragged_distance()
+#==========================================================#
+#=================== Play Arrow Sound =====================#
+#==========================================================#
+
+func play_arrow_sound() -> void:
+	if(was_dragged()):
+		if(arrow_sound_player.playing == false):
+			arrow_sound_player.play()
 
 #==========================================================#
 #===================== SCREEN EXITED ======================#
